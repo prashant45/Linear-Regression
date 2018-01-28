82be7847ba1d1ff3af5de765690bb6b7afa32c2c
prashant45 <prashant123219@gmail.com>
2018-01-28 15:01:51 +0100

    Added plot for cost in gradient descent

--- linear_regression.py
+++ linear_regression.py
@@ -1,27 +1,29 @@
-# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 14:10:45 2018
+
+@author: Prashant
+@github: github.com/prashant45
+"""
 
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
 import util_functions
 
 # Load data
-trainig_data = util_functions.load_data('./data_set_1/ex1data1.txt');
-X_train = trainig_data[0]
-Y_train = trainig_data[1]
+trainig_data = util_functions.load_data('./data_set_1/ex1data1.txt')
+X_train = trainig_data[0] #Population 
+Y_train = trainig_data[1] #Profit of food truck business
 m = len(X_train)
 
 #Plot the data
 util_functions.scatter_plot_data(X_train[:,1], Y_train)
 
 #Some gradient descent settings
-theta = np.zeros(2);
-iterations = 1500;
-alpha = 0.01;
+theta = np.zeros(2)
+iterations = 1500
+alpha = 0.001
 
 #Compute Initial Cost
-J = util_functions.computeCost(X_train, Y_train, theta, m);
+J = util_functions.computeCost(X_train, Y_train, theta, m)
 
 #Run gradient descent algorithm
 algorithm_param = util_functions.gradient_descent(X_train, Y_train, theta, alpha)
@@ -32,8 +34,11 @@ J_history = algorithm_param[1]
 fitted_line = X_train.dot(theta)
 util_functions.linear_fit_plot(X_train[:,1], fitted_line)
 
+#Plot the cost function
+util_functions.cost_function_plot(J_history)
+
 #Predit 
 X_test = 3.5
 predict = theta.dot(np.array([1, X_test]))
-printResult = 'For the population of ' + str(X_test*10000) + ' the model predits a profit of ' + str(predict*10000)
+printResult = 'For the population of ' + str(X_test*10000) + ' the model predits the food truck profit of ' + str(predict*10000)
 print(printResult)
--- util_functions.py
+++ util_functions.py
@@ -1,12 +1,16 @@
-# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 14:10:45 2018
+
+@author: Prashant
+@github: github.com/prashant45
+"""
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 def load_data(file_path):
-    data_train = pd.read_csv(file_path, header=None);
+    data_train = pd.read_csv(file_path, header=None)
     data_train.columns = ['input', 'output']
     data_train['extra feature'] = 1
     
@@ -16,15 +20,22 @@ def load_data(file_path):
     return [X_train, Y_train]
 
 def scatter_plot_data(X, Y):
-	plt.scatter(X, Y, label="Data", color="red")
-	plt.xlabel('Input')
-	plt.ylabel('Output')
-	plt.title('Linear Regression')
+    plt.figure()
+    plt.scatter(X, Y, label="Data", color="red")
+    plt.xlabel('Population: (*10,000)')
+    plt.ylabel('Profit: (*10,000 Euros)')
+    plt.title('Linear Regression')
     
 def linear_fit_plot(X, Y):
-	plt.plot(X, Y, label='Linear Fit', color='blue')
-	plt.legend()
-	plt.show()
+    plt.plot(X, Y, label='Linear Fit', color='blue')
+    plt.legend()
+    
+def cost_function_plot(cost):
+    plt.figure()
+    plt.plot(cost, label='Cost')
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('Cost')
+    plt.title('Cost in Gradient Descent')    
     
 def computeCost(X, Y, theta, m):
     sum = 0;
@@ -47,16 +58,9 @@ def gradient_descent(X, Y, theta, alpha):
             A += common_sum*X[j,0]
             B += common_sum*X[j,1]
             
-        theta[0] -= (alpha*A)/m;
-        theta[1] -= (alpha*B)/m;
+        theta[0] -= (alpha*A)/m
+        theta[1] -= (alpha*B)/m
         
         J_history[i] = computeCost(X, Y, theta, m)
         
     return theta, J_history
-        
-        
-        
-        
-        
-        
-        
