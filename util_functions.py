2e4148166d9bb426019e83666cfd1c38fb00a674
prashant45 <prashant123219@gmail.com>
2018-02-02 13:32:38 +0100

    updated plot functions

--- util_functions.py
+++ util_functions.py
@@ -11,42 +11,47 @@ import matplotlib.pyplot as plt
 
 def load_data(file_path):
     data_train = pd.read_csv(file_path, header=None)
+    l = len(data_train)
     data_train.columns = ['input', 'output']
     data_train['extra feature'] = 1
     
-    X_train = data_train[['extra feature', 'input']].as_matrix()
-    Y_train = data_train['output'].as_matrix()
+    X_train = np.asarray(data_train[['extra feature', 'input']]).reshape(l,2)
+    Y_train = np.asarray(data_train['output']).reshape(l,1)
     
     return [X_train, Y_train]
 
 def scatter_plot_data(X, Y):
     plt.figure()
-    plt.scatter(X, Y, label="Data", color="red")
+    plt.scatter(X, Y, label='Data', color='red')
     plt.xlabel('Population: (*10,000)')
     plt.ylabel('Profit: (*10,000 Euros)')
     plt.title('Linear Regression')
     
-def linear_fit_plot(X, Y):
-    plt.plot(X, Y, label='Linear Fit', color='blue')
-    plt.legend()
+def linear_fit_plot(X, Y, phase):
+    if (phase == 'training'):
+        plt.plot(X, Y, label='Linear Fit', color='blue')
+        plt.legend()
+    elif (phase == 'testing'):
+        plt.plot(X, Y, label='Linear Fit Predictions', color='blue')
+        plt.legend()
     
 def cost_function_plot(cost):
     plt.figure()
     plt.plot(cost, label='Cost')
     plt.xlabel('Number of Iterations')
     plt.ylabel('Cost')
-    plt.title('Cost in Gradient Descent')    
+    plt.title('Cost in Gradient Descent')
     
 def computeCost(X, Y, theta, m):
     sum = 0;
     for i in range(m):
-        sum += (theta.dot(X[i]) - Y[i]) ** 2
+        sum += (np.dot(theta, X[i]) - Y[i]) ** 2
     
     return sum/(2*m)
 
 def gradient_descent(X, Y, theta, alpha):
     iterations = 1500
-    J_history = np.zeros(iterations)
+    J_history = np.zeros(iterations).reshape(iterations, 1)
     m = len(Y)
     
     for i in range(iterations):
@@ -58,8 +63,8 @@ def gradient_descent(X, Y, theta, alpha):
             A += common_sum*X[j,0]
             B += common_sum*X[j,1]
             
-        theta[0] -= (alpha*A)/m
-        theta[1] -= (alpha*B)/m
+        theta[0,0] -= (alpha*A)/m
+        theta[0,1] -= (alpha*B)/m
         
         J_history[i] = computeCost(X, Y, theta, m)
         
